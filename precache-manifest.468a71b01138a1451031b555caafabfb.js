50e7fd63bf50951bb54f9dc194f69339398a575b
venkatzgithub <imvenkatprasad@gmail.com>
2020-05-30 13:34:39 +0900

    Updates

---
+++ precache-manifest.468a71b01138a1451031b555caafabfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54ed768d6ccfdefb9519a71877e33ff",
+    "url": "/RObotFrnz-react/index.html"
+  },
+  {
+    "revision": "c86b9203f1ac5e059711",
+    "url": "/RObotFrnz-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b4a5565b91e39aea1c4f",
+    "url": "/RObotFrnz-react/static/css/main.6f60ebcb.chunk.css"
+  },
+  {
+    "revision": "c86b9203f1ac5e059711",
+    "url": "/RObotFrnz-react/static/js/2.848ac030.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RObotFrnz-react/static/js/2.848ac030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a5565b91e39aea1c4f",
+    "url": "/RObotFrnz-react/static/js/main.7725810e.chunk.js"
+  },
+  {
+    "revision": "098b0919340020e28ba2",
+    "url": "/RObotFrnz-react/static/js/runtime-main.dc9a4142.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RObotFrnz-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
